a8b6b97677d03a374dc05b856dbf6336d1960708
pushlang <aapust@gmail.com>
2020-09-08 22:56:08 +0300

    Initial

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	url := "http://127.0.0.1:8000/send"
 	fmt.Println("URL:>", url)
 
-	var jsonStr = []byte("{\"text\":\"" + os.Args[1] + "\"}")
+	var jsonStr = []byte(`{"text":"` + os.Args[1] + `"}`)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("X-Custom-Header", "myvalue")
 	req.Header.Set("Content-Type", "application/json")
