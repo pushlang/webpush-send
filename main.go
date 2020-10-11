9c3c837cbd9aa490c2bd4cc1b73378c4ee38389d
pushlang <aapust@gmail.com>
2020-10-11 17:45:35 +0300

    i001

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"os"
+	"flag"
 	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
+	"github.com/pushlang/webpush"
 )
 
 func main() {
+	isSrv := flag.Bool("r", false, "Run server")
+	mess := flag.String("m", "Test message!", "Message text")
+	
+	flag.Parse()
+	
+	if *isSrv {
+		fmt.Println("webpush.Run()")
+		os.Exit(webpush.Run())
+	}
+	
 	url := "http://127.0.0.1:8000/send"
 	fmt.Println("URL:>", url)
 
-	var jsonStr = []byte(`{"text":"` + os.Args[1] + `"}`)
+	var jsonStr = []byte(`{"text":"` + *mess + `"}`)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("X-Custom-Header", "myvalue")
 	req.Header.Set("Content-Type", "application/json")
